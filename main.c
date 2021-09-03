383ddf32c3dad49bfc7e8f7b63ce8e0aef619cce
JulianRuival <julian.i.ruival@gmail.com>
2021-09-03 00:22:40 -0300

    Entrega TP1

--- main.c
+++ main.c
@@ -2,33 +2,32 @@
 #include <stdio.h>
 #define EXIT_SUCCESS 0
 
-int main (void) {
-
-    char *cadena_token = NULL;
+int main (void) 
+{
+    char *cadenaLeida = NULL;
 
     token_t token;
 
-    do {
-
-        token = get_token( cadena_token );
-
-        switch (token) {
+    do 
+    {
+        token = get_token(cadenaLeida);
+        switch (token) 
+        {
             case FDT :
                 printf("Fin De Texto:");
                 break;
             case SEP :
-                printf("Separador: %s",cadena_token);
+                printf("Separador: %s",cadenaLeida);
                 break;
             case CAD :
-                printf("Cadena: %s",cadena_token);
+                printf("Cadena: %s",cadenaLeida);
                 break;
             default:
                 printf("Token Desconocido:");
                 break;
         }
 
-    } while ( token != FDT );
-
+    } while (token != FDT);
 
     return EXIT_SUCCESS;
 }
